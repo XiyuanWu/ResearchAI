27770f8959293fa38fddbfa3cc8d4be7b7ef2466
XiyuanWu <xiyuan.24706826@gmail.com>
2026-09-17 22:02:18 -0700

    Backend: Mock Response

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-
+# 3.2 Backend
 def chat_page(request):
     return render(request, "chat.html")
+
+def chat_api(request):
+    return JsonResponse({"message": "You said something"})
