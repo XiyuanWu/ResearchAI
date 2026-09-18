df86df5bc3bed56a30b93e6947e65e9ddc203226
XiyuanWu <xiyuan.24706826@gmail.com>
2026-09-17 23:00:42 -0700

    Minor bug fix

--- chat/services.py
+++ chat/services.py
@@ -3,8 +3,8 @@ from google import genai
 
 # 4.2 LLM Interaction (sent prompt to model)
 # user sent a text to gemini and gemini return a text
-def generate_reponse(message: str) -> str:
-    if not settings.GEMINI_API_key:
+def generate_reponses(message: str) -> str:
+    if not settings.GEMINI_API_KEY:
         raise ValueError("GEMINI_API_KEY is missing")
 
     client = genai.Client(api_key=settings.GEMINI_API_KEY)
