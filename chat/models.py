df9d04d44f7094eefecfb2bb26eb92a736f06101
XiyuanWu <xiyuan.24706826@gmail.com>
2026-09-19 22:39:00 -0700

    Chat Storage (database model)

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# 5.1 Chat Storage (database model)
+# store all conversion list
+class Conversation(models.Model):
+    title = models.CharField(max_length=200, default="new chat")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# store single conversation chat history
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
+    resolve = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
