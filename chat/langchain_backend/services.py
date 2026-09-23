3b15b024ed46a3cd091523b2537f5b8e4c6523b1
XiyuanWu <xiyuan.24706826@gmail.com>
2026-09-22 20:16:27 -0700

    Graph Basics (edges)

--- chat/langchain_backend/services.py
+++ chat/langchain_backend/services.py
@@ -3,10 +3,12 @@ from langchain.agents import create_agent
 from langchain_core.messages import AIMessage, HumanMessage
 # from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
+from langgraph.graph import START, StateGraph
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 from chat.langchain_backend.tools import TOOLS
 from chat.langchain_backend.rag import get_retriever
+from chat.langchain_backend.nodes import model_node, retrieve_node
 
 # 5.3 + 8.1 Prompting
 SYSTEM_PROMPT = """
@@ -80,6 +82,12 @@ def generate_response(message: str, previous_message: list | None = None) -> str
         raise ValueError("Empty response from model")
     return text
 
+# 9.1 Graph Basics (edges)
+graph = StateGraph(dict)
+graph.add_node("retrieve", retrieve_node)
+graph.add_node("model", model_node)
+graph.add_edge(START, "retrieve")
+graph.add_edge("retrieve", "model")
 
 # # 8.2 Tools and Agents (langChain agents)
 # def generate_response(message: str, previous_message: list | None = None) -> str:
